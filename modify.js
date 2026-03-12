5fda3d579a96ee044980c1e1049b0a382237270c
gaorfamily26-a11y <gaorfamily26@gmail.com>
2026-03-12 12:53:59 -0500

    Delete modify.js

--- modify.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-let content = fs.readFileSync('src/components/RegistrationForm.tsx', 'utf8');
-
-// Replace window.open with setShowForm(true)
-content = content.replace(/window\.open\('https:\/\/wa\.me\/yournumber', '_blank'\)/g, 'setShowForm(true)');
-
-fs.writeFileSync('src/components/RegistrationForm.tsx', content);
-console.log('Modified successfully');
